015a440a00d13788e8b6d68250a1730c327c3521
Martin Komischke <martin.komischke@outlook.com>
2019-04-11 17:01:55 +0200

    finish event deserialization

--- src/main/scala/com/maze/Event.scala
+++ src/main/scala/com/maze/Event.scala
@@ -19,9 +19,8 @@ case class PrivateMessageEvent(seqNo: Int, from: Int, to: Int) extends Event(seq
   def toId = to
 }
 
-case class StatusUpdateEvent(seqNo: Int, from: Int, to: Int) extends Event(seqNo) {
+case class StatusUpdateEvent(seqNo: Int, from: Int) extends Event(seqNo) {
   def fromId = from
-  def toId = to
 }
 
 case class BroadcastEvent(seqNo: Int) extends Event(seqNo) {}
@@ -35,6 +34,10 @@ object EventDeserializer {
   def deserialize(parts: Array[String]): Event = {
     val event = parts(0) match {
       case "F" => FollowEvent(parts(1).toInt, parts(2).toInt, parts(3).toInt)
+      case "U" => UnFollowEvent(parts(1).toInt, parts(2).toInt, parts(3).toInt)
+      case "P" => PrivateMessageEvent(parts(1).toInt, parts(2).toInt, parts(3).toInt)
+      case "S" => StatusUpdateEvent(parts(1).toInt, parts(2).toInt)
+      case "B" => BroadcastEvent(parts(1).toInt)
     }
     return event
   }
--- src/test/scala/com/maze/EventDeserializerTests.scala
+++ src/test/scala/com/maze/EventDeserializerTests.scala
@@ -3,13 +3,38 @@ package com.maze
 import org.scalatest.FunSuite
 
 class EventDeserializerTests extends FunSuite {
-  test("Instanciate event") {
+  test("Event object should provide sequence number") {
     val event = new FollowEvent(22, 33, 44)
     assert(event.sequenceNumber == 22)
   }
 
-  test("Deserialize event from raw string") {
+  test("Deserialize event should private correct sequence number") {
     val event = EventDeserializer.deserialize("F|22|44|55")
     assert(event.sequenceNumber == 22)
+  }
+
+  test("Deserialize event should be of type FollowEvent") {
+    val event = EventDeserializer.deserialize("F|22|44|55")
+    assert(event.isInstanceOf[FollowEvent])
+  }
+
+  test("Deserialize event should be of type UnFollowEvent") {
+    val event = EventDeserializer.deserialize("U|22|44|55")
+    assert(event.isInstanceOf[UnFollowEvent])
+  }
+
+  test("Deserialize event should be of type PrivateMessageEvent") {
+    val event = EventDeserializer.deserialize("P|22|44|55")
+    assert(event.isInstanceOf[PrivateMessageEvent])
+  }
+
+  test("Deserialize event should be of type StatusUpdateEvent") {
+    val event = EventDeserializer.deserialize("S|22|44")
+    assert(event.isInstanceOf[StatusUpdateEvent])
+  }
+
+  test("Deserialize event should be of type BroadcastEvent") {
+    val event = EventDeserializer.deserialize("B|22")
+    assert(event.isInstanceOf[BroadcastEvent])
   }
 }

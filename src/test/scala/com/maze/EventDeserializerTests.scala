8b94388afa9e836d1031740dc300d8a834392bd2
Martin Komischke <martin.komischke@outlook.com>
2019-04-12 08:39:34 +0200

    remove obsolete new

--- src/test/scala/com/maze/EventDeserializerTests.scala
+++ src/test/scala/com/maze/EventDeserializerTests.scala
@@ -4,7 +4,7 @@ import org.scalatest.FunSuite
 
 class EventDeserializerTests extends FunSuite {
   test("Event object should provide sequence number") {
-    val event = new FollowEvent(22, 33, 44)
+    val event = FollowEvent(22, 33, 44)
     assert(event.sequenceNumber == 22)
   }
 

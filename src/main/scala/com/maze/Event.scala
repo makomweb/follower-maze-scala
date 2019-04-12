1a22cae11e293b00848b022b9dbad375802fef55
Martin Komischke <martin.komischke@outlook.com>
2019-04-12 14:35:26 +0200

    implement raising an event

--- src/main/scala/com/maze/Event.scala
+++ src/main/scala/com/maze/Event.scala
@@ -2,33 +2,44 @@ package com.maze
 
 abstract class Event(seqNo: Int) {
   def sequenceNumber = seqNo
+  def raiseEvent(userRepository: UserRepository)
 }
 
 case class FollowEvent(seqNo: Int, from: Int, to: Int) extends Event(seqNo) {
   def fromId = from
   def toId = to
   override def toString = s"$sequenceNumber|F|$fromId|$toId"
+
+  override def raiseEvent(userRepository: UserRepository): Unit = ???
 }
 
 case class UnFollowEvent(seqNo: Int, from: Int, to: Int) extends Event(seqNo) {
   def fromId = from
   def toId = to
   override def toString = s"$sequenceNumber|U|$fromId|$toId"
+
+  override def raiseEvent(userRepository: UserRepository): Unit = ???
 }
 
 case class PrivateMessageEvent(seqNo: Int, from: Int, to: Int) extends Event(seqNo) {
   def fromId = from
   def toId = to
   override def toString = s"$sequenceNumber|P|$fromId|$toId"
+
+  override def raiseEvent(userRepository: UserRepository): Unit = ???
 }
 
 case class StatusUpdateEvent(seqNo: Int, from: Int) extends Event(seqNo) {
   def fromId = from
   override def toString = s"$sequenceNumber|S|$fromId"
+
+  override def raiseEvent(userRepository: UserRepository): Unit = ???
 }
 
 case class BroadcastEvent(seqNo: Int) extends Event(seqNo) {
   override def toString = s"$sequenceNumber|B"
+
+  override def raiseEvent(userRepository: UserRepository): Unit = ???
 }
 
 object EventDeserializer {
--- src/main/scala/com/maze/EventQueueProcessor.scala
+++ src/main/scala/com/maze/EventQueueProcessor.scala
@@ -2,6 +2,18 @@ package com.maze
 
 class EventQueueProcessor(userRepository: UserRepository, eventQueue: EventQueue) extends Runnable {
   override def run(): Unit = {
-    
+    while (true) {
+      if (eventQueue.nonEmpty) {
+        val event = eventQueue.dequeue()
+        try {
+          event.raiseEvent(userRepository)
+        } catch {
+          case ex: RuntimeException => {
+            println("Exception while raising event!")
+            println(ex)
+          }
+        }
+      }
+    }
   }
 }

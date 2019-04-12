fd1757af759a0c2823ff37660d61e59840a31d46
Martin Komischke <martin.komischke@outlook.com>
2019-04-12 14:26:06 +0200

    implement user client socket server and user client processor

---
+++ src/main/scala/com/maze/UserClientProcessor.scala
@@ -0,0 +1,29 @@
+package com.maze
+
+import java.io.{BufferedReader, IOException, InputStreamReader}
+import java.net.Socket
+
+class UserClientProcessor(socket: Socket, userRepository: UserRepository) extends Runnable {
+  override def run(): Unit = {
+    try {
+      val stream = socket.getInputStream()
+      val reader = new InputStreamReader(stream)
+      val bufferedReader = new BufferedReader(reader)
+      while (true) {
+        val line = bufferedReader.readLine()
+        if (line != null) {
+          userRepository.add(line.toInt, socket)
+        }
+      }
+    } catch {
+      case ex: IOException => {
+        println("Caught exception while processing accepted users!")
+        println(ex)
+      }
+      case ex: RuntimeException => {
+        println("Could not add accepted user!")
+        println(ex)
+      }
+    }
+  }
+}
---
+++ src/main/scala/com/maze/UserClientSocketServer.scala
@@ -0,0 +1,22 @@
+package com.maze
+
+import java.io.IOException
+import java.net.ServerSocket
+import java.util.concurrent.ExecutorService
+
+class UserClientSocketServer(serverSocket: ServerSocket, threadPool: ExecutorService, userRepository: UserRepository) extends Runnable {
+  override def run(): Unit = {
+    while (true) {
+      try {
+        val socket = serverSocket.accept()
+        val processor: UserClientProcessor = new UserClientProcessor(socket, userRepository)
+        threadPool.submit(processor)
+      } catch {
+        case ex: IOException => {
+          println("Caught exception while accepting user connections!")
+          println(ex)
+        }
+      }
+    }
+  }
+}

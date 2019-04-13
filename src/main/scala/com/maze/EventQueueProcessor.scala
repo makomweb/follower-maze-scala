8066a54ef7fe6f79d883f78ca2e4e05e91efc4fa
Martin Komischke <martin.komischke@outlook.com>
2019-04-13 13:53:46 +0200

    make EventQueueProcessor less error prone

--- src/main/scala/com/maze/EventQueueProcessor.scala
+++ src/main/scala/com/maze/EventQueueProcessor.scala
@@ -4,22 +4,21 @@ class EventQueueProcessor(userRepository: UserRepository, eventQueue: EventQueue
   override def run(): Unit = {
     var sequenceNumber = 1
     while (true) {
-      if (eventQueue.nonEmpty) {
-        var event = eventQueue.peek()
-        if (event != null && event.sequenceNumber <= sequenceNumber) {
-          event = eventQueue.dequeue()
-          sequenceNumber = sequenceNumber + 1
-          try {
+      try {
+        if (eventQueue.nonEmpty) {
+          var event = eventQueue.peek
+          if (event != null && event.sequenceNumber <= sequenceNumber) {
+            event = eventQueue.dequeue()
+            sequenceNumber = sequenceNumber + 1
             event.raiseEvent(userRepository)
             print("Raised event ")
             println(event)
-
-          } catch {
-            case ex: RuntimeException => {
-              println("Exception while raising event!")
-              println(ex)
-            }
           }
+        }
+      } catch {
+        case ex: RuntimeException => {
+          println("Exception while raising event!")
+          println(ex)
         }
       }
     }

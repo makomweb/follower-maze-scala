e9179e650d2de6a68aebbe0a76a1f103f5da5842
Martin Komischke <martin.komischke@outlook.com>
2019-04-14 12:57:29 +0200

    activate event queue processor

--- src/main/scala/com/maze/EventQueueProcessor.scala
+++ src/main/scala/com/maze/EventQueueProcessor.scala
@@ -4,22 +4,22 @@ class EventQueueProcessor(userRepository: UserRepository, eventQueue: EventQueue
   override def run(): Unit = {
     var sequenceNumber = 1
     while (true) {
-      //Thread.sleep(1000L)
+      //Thread.sleep(10L)
       //print(".")
       try {
         if (eventQueue.nonEmpty) {
+          /*
           val event = eventQueue.dequeue
           sequenceNumber = sequenceNumber + 1
-          /*
-          val event = eventQueue.peek
-          if (event != null && event.sequenceNumber <= sequenceNumber) {
-            //event = eventQueue.dequeue()
-            println(s"sequence number: $sequenceNumber")
-            sequenceNumber = sequenceNumber + 1
-            //event.raiseEvent(userRepository)
-          }
           */
-          event.raiseEvent(userRepository)
+          var event = eventQueue.peek
+          if (event != null && event.sequenceNumber <= sequenceNumber) {
+            event = eventQueue.dequeue
+            //println(s"sequence number: $sequenceNumber")
+            sequenceNumber = sequenceNumber + 1
+            event.raiseEvent(userRepository)
+          }
+          //event.raiseEvent(userRepository)
           //println (s"Raised event: $event")
         }
       } catch {
